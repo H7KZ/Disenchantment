00a0db9e7e34e455a2fbdbafb797060feb39a2cd
H7KZ <74021016+H7KZ@users.noreply.github.com>
2023-12-17 17:30:46 +0100

    fix repair cost setting

--- src/main/java/cz/kominekjan/disenchantment/events/normal/DisenchantmentNormalEvent.java
+++ src/main/java/cz/kominekjan/disenchantment/events/normal/DisenchantmentNormalEvent.java
@@ -8,14 +8,21 @@ import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 
 import java.util.Map;
 
+import static cz.kominekjan.disenchantment.events.DisenchantmentEvent.countAnvilCost;
+
 public class DisenchantmentNormalEvent {
 
     public static void onDisenchantmentEvent(PrepareAnvilEvent e, Map<Enchantment, Integer> enchantments) {
         ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
+
         EnchantmentStorageMeta esm = (EnchantmentStorageMeta) book.getItemMeta();
+
         enchantments.forEach((en, l) -> esm.addStoredEnchant(en, l, true));
+
         book.setItemMeta(esm);
 
         e.setResult(book);
+
+        e.getInventory().setRepairCost(countAnvilCost(enchantments));
     }
 }
