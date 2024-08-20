f5fa77410fca8aea44e158fe39bfdd3aba7d5c28
alexcrea <42614139+alexcrea@users.noreply.github.com>
2024-08-20 13:38:06 +0200

    remove the need of NBTEditor by using spigot api.

--- src/main/java/cz/kominekjan/disenchantment/libs/nbteditor/NBT.java
+++
@@ -1,9 +0,0 @@
-package cz.kominekjan.disenchantment.libs.nbteditor;
-
-import org.bukkit.inventory.ItemStack;
-
-public class NBT {
-    public static ItemStack setNBTRepairCost(ItemStack item, int repairCost) {
-        return NBTEditor.set(item, repairCost, "RepairCost");
-    }
-}
