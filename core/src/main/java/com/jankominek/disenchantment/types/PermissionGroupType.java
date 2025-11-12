c5e14bfbb33c7eb0dcce7b1d192c639def5e2e5c
H7KZ <74021016+H7KZ@users.noreply.github.com>
2025-11-12 21:05:30 +0100

    fix: permission diagnostic command

--- core/src/main/java/com/jankominek/disenchantment/types/PermissionGroupType.java
+++ core/src/main/java/com/jankominek/disenchantment/types/PermissionGroupType.java
@@ -84,13 +84,7 @@ public enum PermissionGroupType {
     ),
     COMMAND_DIAGNOSTIC(
             PermissionType.COMMAND_ALL,
-            PermissionType.COMMAND_DIAGNOSTIC,
-            // We want diagnosis permission to be very lenient about what permission to use
-            // So most of the time we allow admin with only some permission to execute the diagnostic too
-            PermissionType.ALL,
-            PermissionType.ANVIL_ALL,
-            PermissionType.DEPRECATED_ANVIL_ALL,
-            PermissionType.GUI_ALL
+            PermissionType.COMMAND_DIAGNOSTIC
     ),
 
     GUI(
