4653b7336245d8edbd8ca7cd2c26147a2b48e18b
H7KZ <74021016+H7KZ@users.noreply.github.com>
2026-06-08 22:23:44 +0200

    feat: implement DisenchantmentPlaceholderExpansion

---
+++ core/src/main/java/com/jankominek/disenchantment/plugins/placeholderapi/DisenchantmentPlaceholderExpansion.java
@@ -0,0 +1,41 @@
+package com.jankominek.disenchantment.plugins.placeholderapi;
+
+import com.jankominek.disenchantment.config.Config;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
+
+import static com.jankominek.disenchantment.Disenchantment.plugin;
+
+public class DisenchantmentPlaceholderExpansion extends PlaceholderExpansion {
+
+    @Override
+    public String getIdentifier() {
+        return "disenchantment";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "H7KZ";
+    }
+
+    @Override
+    public String getVersion() {
+        return plugin.getDescription().getVersion();
+    }
+
+    @Override
+    public boolean persist() {
+        return true;
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer player, String params) {
+        return switch (params.toLowerCase()) {
+            case "enabled"            -> String.valueOf(Config.isPluginEnabled());
+            case "disenchant_enabled" -> String.valueOf(Config.Disenchantment.isEnabled());
+            case "shatter_enabled"    -> String.valueOf(Config.Shatterment.isEnabled());
+            case "version"            -> plugin.getDescription().getVersion();
+            default                   -> null;
+        };
+    }
+}
